8a259f03616a82f1e5ff30ba5f9f29f21a2d9988
LilianaWarlock <148593235+LilianaWarlock@users.noreply.github.com>
2026-08-15 07:19:00 -0400

    Soaring effect
    if i hadn't forgotten to add this, i wouldn't have wasted an hour of my life

--- minecraft/kubejs/startup_scripts/main.js
+++ minecraft/kubejs/startup_scripts/main.js
@@ -1,3 +1,14 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded startup example script)')
+StartupEvents.registry("mob_effect", (event) => {
+  event
+    .create("soaring") // Create the effect under "kubejs:custom_effect"
+    .color(0x000000) // Sets the color of the Effect's Particles.
+    .beneficial() // Categorizes the Effect as Beneficial.
 
+    // modifyAttribute is useful to scale an entity's attributes only lasting while under the effect
+    .modifyAttribute(
+      "air_drag_modifier", // The attribute to scale
+      "ea9a4c23-07b1-45cf-aa0d-5fd7232fc875", //Some random UUID which serves as the effect's unique instance
+      -0.1, // The amount to increase/decrease by
+      "add_value", // The operation to perform
+    );
+});
